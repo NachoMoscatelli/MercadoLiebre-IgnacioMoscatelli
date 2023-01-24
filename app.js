8d9594d241ed0a84aa161ecdd90fcf4ab8c75ddb
Ignacio Moscatelli <inm300300@gmail.com>
2023-01-24 18:55:57 -0300

    Retoques

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 });
 
+app.post('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+});
+
 app.get('/register', (req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
 });
